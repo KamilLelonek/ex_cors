3aace70e961c385978c55db78942910c436279ad
Kamil Lelonek <squixy.sln@gmail.com>
2017-07-14 13:38:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# ex_cors
+
+A Plug-based CORS middleware.
+
+## Installation
+
+The package can be installed by adding `ex_cors` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:ex_cors, "~> 0.1.0"},
+    # ...
+  ]
+end
+```
+
+After you're done, run `mix deps.get` in your terminal to fetch and compile all dependencies.
+
+## Usage
---
+++ mix.exs
@@ -0,0 +1,46 @@
+defmodule ExCors.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app:             :ex_cors,
+      version:         "0.1.0",
+      elixir:          "~> 1.4",
+      build_embedded:  Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description:     description(),
+      package:         package(),
+      deps:            deps(),
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.16.2", runtime: false},
+      {:plug, "~> 1.3"},
+    ]
+  end
+
+  defp description do
+    """
+      A Plug-based CORS middleware.
+    """
+  end
+
+  defp package do
+    [
+     name:        :ex_cors,
+     files:       ["lib", "config", "mix.exs", "README.md"],
+     maintainers: ["Kamil Lelonek"],
+     licenses:    ["Apache 2.0"],
+     links: %{
+       "GitHub" => "https://github.com/KamilLelonek/ex_cors",
+       "Docs"   => "https://hexdocs.pm/ex_cors/",
+     },
+    ]
+  end
+end
